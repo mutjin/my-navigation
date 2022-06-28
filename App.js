8f2a36670978cb825034f3556b13c32ca69b2fce
mutjin <104980932+mutjin@users.noreply.github.com>
2022-06-28 15:49:17 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ function DetailScreen({navigation}){
             <Button title="Go to DetailScreen again" onPress={()=>navigation.push('Detail')}/>
             <Button title="Go to Home" onPress={()=>navigation.navigate('Home')}/>
             <Button title="Go back" onPress={()=>navigation.goBack()}/>
+            <Button title="Go back to first screen in stack" onPress={()=>navigation.popToTop()}/>
         </View>
     )
 }
